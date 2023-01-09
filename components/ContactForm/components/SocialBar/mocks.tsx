e2105891820c27e789527248f9eddf18df16b93a
rafayfromfast <rafay.abbas.malik@gmail.com>
2023-01-09 11:27:13 +0500

    update footer and bottom bar allignment

--- components/ContactForm/components/SocialBar/mocks.tsx
+++ components/ContactForm/components/SocialBar/mocks.tsx
@@ -6,6 +6,6 @@ export const socials = [
     link: "https://www.facebook.com/profile.php?id=100088359412691&sk=about",
     icon: <Facebook className={styles.icon} />,
   },
-  { link: "./twitter", icon: <Twitter className={styles.icon} /> },
-  { link: "./linkedIn", icon: <Linkedin className={styles.icon} /> },
+  { link: "#", icon: <Twitter className={styles.icon} /> },
+  { link: "#", icon: <Linkedin className={styles.icon} /> },
 ];
